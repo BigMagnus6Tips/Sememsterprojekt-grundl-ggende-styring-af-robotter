739862eb2cdb37710f696f693bab62fbb441c624
Lukas Hjeronimus Sørensen <luckyhjeronimus@gmail.com>
2024-11-20 12:46:47 +0100

    readded killswitch after merge

--- main.py
+++ main.py
@@ -28,6 +28,14 @@ async def monitorStart():
             # This is for plotting the time in the csv file.
             current_time += 0.1
 
+
+# KillSwitch function
+def interruption_handler(pin):
+    print("KillSwitch activated")
+    global KillSwitch
+    KillSwitch = True
+
+
 # function to start the program
 async def start():
     print("Starts")
@@ -37,8 +45,11 @@ async def start():
     # await car.inPlaceRotation(180)
     shouldMonitor = False # Then the program stops monitoring.
 
-    while True:
+    global KillSwitch
+    while not KillSwitch:
+        # print(KillSwitchButton.value())
         await joystickcontroller.JoystickMove()
+    
 
 if __name__ == '__main__':
 
@@ -75,4 +86,44 @@ if __name__ == '__main__':
         multiStepper.stop()
 
 
+
+if __name__ == '__main__':
+
+    # Set up the ADC pin (choose one of GP26, GP27, or GP28 for ADC on Pico W)
+    adc_pin = ADC(Pin(26))  # GP26 is labeled as ADC0 on Pico found in the Kicad drawing
+
+    # Reference voltage for the Pico W is typically 3.3V
+    REFERENCE_VOLTAGE = 3.3
+    R2 = 2200  # Known resistor value in the voltage divider circuit. We chose this resistor to get a linear graph for the LDR, so that we could get as big of a span as possible.
+
+
+
+    print("Starts")
+
+    # Makes objects for the motor
+    motorRight = StepperMotor([0,1,2,3], 0.15, 18000, StepperMotor.half_step)
+    motorLeft = StepperMotor([4,5,6,7], 0.15, 18000, StepperMotor.half_step)
+
+    # makes multistepper object with the motors
+    multiStepper = MultiStepper([motorLeft, motorRight])
+
+    # set their delays
+    multiStepper.set_Delays([0.01,0.01])
+
+    # makes a differentialDriver object
+    car = DifferentialDriver(multiStepper)
+
+    joystickcontroller = JoystickController()
+
+    # First KillSwitch is set to False, but if the button connected to pin 22 is pressed it is set to True and interrupts the program.
+    KillSwitch = False
+    KillSwitchButton = Pin(22, Pin.IN, Pin.PULL_UP)
+    KillSwitchButton.irq(trigger=Pin.IRQ_FALLING, handler=interruption_handler)
     
+    sleep(1)
+    try:
+        uasyncio.run(start())
+    except KeyboardInterrupt:
+        sleep(1)
+        multiStepper.stop()
+        print("Program stopped by user")
