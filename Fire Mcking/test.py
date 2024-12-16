c321bcbe7b1ddee41c973620e1a4348b5b952853
Lukas Hjeronimus Sørensen <luckyhjeronimus@gmail.com>
2024-12-16 16:11:55 +0100

    added kicad flip flop thing

--- Fire Mcking/test.py
+++ Fire Mcking/test.py
@@ -1,14 +1,13 @@
 from machine import Pin, ADC
 from time import sleep
 
-inputPin1 = Pin(19, Pin.IN, pull=Pin.PULL_DOWN)
-inputPin2 = Pin(26, Pin.IN, pull=Pin.PULL_DOWN)
+greenReadLED = Pin(19, Pin.IN, pull=Pin.PULL_DOWN)
+redReadLED = Pin(27, Pin.IN, pull=Pin.PULL_DOWN)
+resetPin = Pin(18, Pin.OUT)
 #inputPinADC = ADC(Pin(26))
 try:
     while True:
-        #voltage = inputPinADC.read_u16() * 3.3 / 65535
-        print("Pin 19: ", inputPin1.value())
-        print("Pin 26:", inputPin2.value())
-        sleep(0.1)
+        resetPin.toggle()
+        sleep(1)
 except KeyboardInterrupt:
     pass
