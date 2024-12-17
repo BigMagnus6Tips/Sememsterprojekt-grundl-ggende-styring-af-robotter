b6567b6ccf9dd3e55d211a040606052137198a1c
Alexander <alex347a@gmail.com>
2024-12-17 19:42:35 +0100

    almost safe fire mcking

--- Fire Mcking/synkrontMain/main.py
+++ Fire Mcking/synkrontMain/main.py
@@ -10,86 +10,97 @@ R2 = 2200  # Known resistor value in the voltage divider circuit. We chose this 
 THRESHOLD = 0.75  # Threshold value for the LDR to detect black or white. This value is found by testing the LDR on black and white surfaces and finding the average value between the two. This value is used to determine if the LDR is on black or white surface. The value is between 0 and 1, where 1 is the maximum value the LDR can read, which is the reference voltage.
  
 def turnBlackOn(pin):
-    global onBlack
+    global redLED
     #print("On black")
-    onBlack = True
+    redLED = True
     
 def turnBlackOff(pin):
-    global onBlack
+    global redLED
     #print("On white")
-    onBlack = False
+    redLED = False
 
 def turnBlack2On(pin):
-    global onBlack2
+    global greenLED
     #print("On black2")
-    onBlack2 = True
+    greenLED = True
     
 def turnBlack2Off(pin):
-    global onBlack2
+    global greenLED
     #print("On white2")
-    onBlack2 = False
+    greenLED = False
 
 def monitorLDRsync():
-    global onBlack
-    global onBlack2
+    global redLED
+    global greenLED
     if greenLEDInput.value() == 1:
-        onBlack2 = True
+        greenLED = True
     else:
-        onBlack2 = False
+        greenLED = False
     
     if redLEDInput.value() == 1:
-        onBlack = True
+        redLED = True
     else:
-        onBlack = False
-
-def main():
-    global onBlack
-    global onBlack2
-    
-    onBlack2 = False
-    onBlack = False
-    maxSpeed = 600 # 600 er limit for koden ca. ved 30 % PWM
-    resolution = 50
-    timeToMaxSpeed = 0.5
+        redLED = False
+def rampUp(maxSpeed, resolution, timeToMaxSpeed):
     timeForEachResolution = timeToMaxSpeed/resolution
     speedSteps = maxSpeed/resolution
     
-    sleep(1)
-    
-    
     for i in range(1, resolution+1):
         multiStepper.setSyncDelay(1/(i*speedSteps))
-        print(i*speedSteps)
         multiStepper.moveSync([i*speedSteps*timeForEachResolution, i*speedSteps*timeForEachResolution])
+
+def main():
+    global redLED
+    global greenLED
     
-    resetPin.value(1)
-    resetPin.value(0)
+    greenLED = False
+    redLED = False
+    maxSpeed = 600 # 600 er limit for koden ca. ved 30 % PWM
+    hardTurnSpeed = 300
+    resolution = 50
+    timeToMaxSpeed = 0.5
+    
+    sleep(1)
+    
+    rampUp(maxSpeed, resolution, timeToMaxSpeed)
+
+    redResetPin.value(1)
+    redResetPin.value(0)
+    greenResetPin.value(1)
+    greenResetPin.value(0)
     
     multiStepper.setSyncDelay(1/maxSpeed)
     
     leftMotorAmountHard = 0
-    rightMotorAmountHard = 2
+    rightMotorAmountHard = 3
     
     leftMotorAmountSoft = 0
-    rightMotorAmountSoft = 2
+    rightMotorAmountSoft = 4
     counter = 0
     while True:
         monitorLDRsync()
-        if onBlack2:
-            while not onBlack:
+        if greenLED:
+            multiStepper.setSyncDelay(1/hardTurnSpeed)
+            while not redLED:
                 monitorLDRsync()
                 multiStepper.moveSync([leftMotorAmountHard,rightMotorAmountHard])
-            resetPin.value(1)
-            resetPin.value(0)
-
-        elif onBlack:
-
+            rampUp(maxSpeed, 5, 0.1)
+            greenResetPin.value(1)
+            greenResetPin.value(0)
+            monitorLDRsync()
+            while not redLED:
+                monitorLDRsync()
+                multiStepper.moveSync([1,0])
+            multiStepper.setSyncDelay(1/maxSpeed)
+        elif redLED:
             multiStepper.moveSync([leftMotorAmountSoft,rightMotorAmountSoft])
+            redResetPin.value(1)
+            redResetPin.value(0)
             
         else:
-            if counter%2 == 0:
-                multiStepper.moveSync([6,5])
-            multiStepper.moveSync([2,2])
+            if counter%3 == 0:
+                multiStepper.moveSync([2,0])
+            multiStepper.moveSync([3,3])
             counter += 1
             #print("turning")
     
@@ -98,8 +109,8 @@ def main():
 if __name__ == '__main__':
 
     # Makes objects for the motor
-    motorRight = StepperMotor(reversed([0,1,2,3]), 0.45, 18000)
-    motorLeft = StepperMotor([4,5,6,7], 0.45, 18000)
+    motorRight = StepperMotor(reversed([0,1,2,3]), 0.30, 18000)
+    motorLeft = StepperMotor([4,5,6,7], 0.30, 18000)
 
 
     # Makes multistepper object with the motors
@@ -107,7 +118,10 @@ if __name__ == '__main__':
     
     redLEDInput = Pin(28, Pin.IN, Pin.PULL_DOWN)
     greenLEDInput = Pin(19, Pin.IN, Pin.PULL_DOWN)
-    resetPin = Pin(18, Pin.OUT)
+    greenResetPin = Pin(18, Pin.OUT)
+    redResetPin = Pin(17, Pin.OUT)
+
+    onBoardLED = Pin(25, Pin.OUT)
 
     try:
         main()
